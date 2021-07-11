cc55dec0a7be8f41daa1f896e2f5c514eb13f363
rohit1299 <75167487+rohit1299@users.noreply.github.com>
2021-07-11 12:11:12 +0530

    Add files via upload

---
+++ Day 14/script.js
@@ -0,0 +1,29 @@
+function doubleClick(){
+    console.log('double clicked');
+}
+
+function contextMenu(){
+    console.log('right click');
+}
+
+function drag(){
+    console.log('dragged');
+}
+
+function dragEnd(){
+    console.log('Drag ended');
+}
+function dragStart(){
+    console.log('Drag started');
+}
+function dragOver(){
+    console.log('Drag over');
+}
+
+function dragLeave(){
+    console.log('Drag leave');
+}
+
+function drop(){
+    console.log('dropped');
+}
