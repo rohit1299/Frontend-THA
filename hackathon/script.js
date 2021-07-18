8a58e7c186c2c0cb38acca1554cb405bb8ed3bb4
rohit1299 <75167487+rohit1299@users.noreply.github.com>
2021-07-18 14:38:04 +0530

    Add files via upload

---
+++ hackathon/script.js
@@ -0,0 +1,10 @@
+var form = document.getElementById("form1")
+
+// function close(){
+//     // form.classlist.add("hide")
+//     console.log("hi");
+// }
+
+document.getElementById("close").addEventListener("click",()=>{
+    form.classList.add("hide")
+})
